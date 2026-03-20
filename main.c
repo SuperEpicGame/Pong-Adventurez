6e8c5573705672e210deb79093fc9fc9b34d67cb
flamungo03 <antongajicolsson03@gmail.com>
2026-03-20 16:10:12 +0100

    Skapade spelblocken för bägge spelarna #2

--- main.c
+++ main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include "player.h"
 
 int main(int argc, char *argv[])
 {
 
     SDL_Window *window = SDL_CreateWindow("Pong", 1000, 800, 0); // Skapar mitt fönster. Namn, bredd, höjd, flaggor.
-    // int test = 0;
+
+    SDL_ShowWindow(window);        // Visa mitt fönster
 
     SDL_Event buttonPressed;    
+    SDL_Renderer *renderer;
+    renderer = SDL_CreateRenderer(window, 0);
+    
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer,255,0,0,255);
+
+    renderPlayer(renderer);
+    SDL_RenderPresent(renderer);
+    
+    //SDL_SetWindowIcon();
 
     while (1)
     {
-        SDL_ShowWindow(window);        // Visa mitt fönster
         SDL_PollEvent(&buttonPressed); // Pollad lösning: har något hänt?
         // scanf("%d", &test);
         // SDL_PollEvent(SDL_EVENT_TEXT_INPUT);
